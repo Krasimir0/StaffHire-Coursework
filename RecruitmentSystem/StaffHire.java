51eab66fed7ac8bf3b3b94c2b26b82421da62e20
Krasimir Petranov <kimim0548@gmail.com>
2025-02-18 11:58:34 +0000

    Add StaffHire class

--- RecruitmentSystem/StaffHire.java
+++ RecruitmentSystem/StaffHire.java
@@ -7,27 +7,89 @@
  */
 public class StaffHire
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class StaffHire
-     */
-    public StaffHire()
-    {
-        // initialise instance variables
-        x = 0;
+    private int vacancyNumber;
+    private int designationType;
+    private String jobType;
+    private String staffName;
+    private String joiningDate;
+    private String qualification;
+    private String appointedBy;
+    private boolean joined;
+    
+    public StaffHire(int vacancyNumber, int designationType, String jobType, String staffName, String joiningDate, String qualification, String appointedBy, boolean joined) {
+        this.vacancyNumber = vacancyNumber;
+        this.designationType = designationType;
+        this.jobType = jobType;
+        this.staffName = staffName;
+        this.joiningDate = joiningDate;
+        this.qualification = qualification;
+        this.appointedBy = appointedBy;
+        this.joined = joined;
+    }
+    
+       public int getVacancyNumber() {
+        return vacancyNumber;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public int getDesignationType() {
+        return designationType;
     }
+
+    public String getJobType() {
+        return jobType;
+    }
+
+    public String getStaffName() {
+        return staffName;
+    }
+
+    public String getJoiningDate() {
+        return joiningDate;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public String getAppointedBy() {
+        return appointedBy;
+    }
+
+    public boolean isJoined() {
+        return joined;
+    }
+
+    // Setters
+    public void setVacancyNumber(int vacancyNumber) {
+        this.vacancyNumber = vacancyNumber;
+    }
+
+    public void setDesignationType(int designationType) {
+        this.designationType = designationType;
+    }
+
+    public void setJobType(String jobType) {
+        this.jobType = jobType;
+    }
+
+    public void setStaffName(String staffName) {
+        this.staffName = staffName;
+    }
+
+    public void setJoiningDate(String joiningDate) {
+        this.joiningDate = joiningDate;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public void setAppointedBy(String appointedBy) {
+        this.appointedBy = appointedBy;
+    }
+
+    public void setJoined(boolean joined) {
+        this.joined = joined;
+    }
+    
 }
